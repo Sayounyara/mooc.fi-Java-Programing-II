f20df520298e149744e1669a92ead065b0f349e2
bitterCell <76990920+bitterCell@users.noreply.github.com>
2022-02-01 21:52:43 +1030

    Add files via upload

---
+++ part12-Part12_07.Die/src/main/java/Die.java
@@ -0,0 +1,17 @@
+
+import java.util.Random;
+
+public class Die {
+
+    private Random random;
+    private int numberOfFaces;
+
+    public Die(int numberOfFaces) {
+        this.random = new Random();
+        this.numberOfFaces = numberOfFaces;
+    }
+
+    public int throwDie() {
+        return random.nextInt(this.numberOfFaces) + 1;
+    }
+}
