2fd8e3ebfa7826d39dc3dd1a2430cdd940c37850
bitterCell <76990920+bitterCell@users.noreply.github.com>
2022-02-08 21:34:40 +1030

    Add files via upload

---
+++ part13-Part13_01.MyFirstApplication/src/main/java/myFirstApplication/MyFirstApplication.java
@@ -0,0 +1,21 @@
+package myFirstApplication;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class MyFirstApplication extends Application {
+    
+    @Override
+    public void start(Stage window) {
+        window.setTitle("My first application");
+        window.show();
+    }
+    
+
+    public static void main(String[] args) {
+        System.out.println("Launched!");
+        launch(MyFirstApplication.class);
+    }
+
+}
