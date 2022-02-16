1fe035c60079bff7fc3d754cff17f4a7b0121da1
bitterCell <76990920+bitterCell@users.noreply.github.com>
2022-02-16 16:19:44 +1030

    Add files via upload

---
+++ part14-Part14_08.Hurray/src/main/java/hurraa/HurraaSovellus.java
@@ -0,0 +1,36 @@
+package hurraa;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.media.AudioClip;
+import javafx.stage.Stage;
+
+public class HurraaSovellus extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane pane = new BorderPane();
+
+        Button nappi = new Button("Hurraa!");
+        pane.setCenter(nappi);
+        
+        // This library was deleted...
+        AudioClip hurray = new AudioClip("file:Applause-Yannick_Lemieux.wav");   
+        
+        nappi.setOnAction((event) -> {
+            hurray.play();
+        });
+
+        Scene scene = new Scene(pane, 600, 400);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
